d132eb0452e165466645b36363b152afa36fcb5e
Eric Stoclet <estoclet.external@epo.org>
2021-01-30 18:57:02 +0100

    Initial commit

---
+++ publicTrem.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#/usr/local/games/tremulous/tremded.x86 +set dedicated 2 +exec server.cfg +set net_port 30721
+#/usr/games/tremfusionded +set dedicated 2 +exec server.cfg +set net_port 30721
+/usr/local/bin/tremded +set dedicated 2 +exec server.cfg +set net_port 30721
+
